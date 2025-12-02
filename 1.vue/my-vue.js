2198da74b91a716a03da6dda9bfb55efff1f76ef
lsr <3266788421@qq.com>
2025-12-02 15:09:45 +0800

    12.2

--- 1.vue/my-vue.js
+++ 1.vue/my-vue.js
@@ -1,127 +1,133 @@
 // 定义一个简单的Vue类，模拟Vue的核心功能
 class Vue {
-  // 构造函数，接收配置选项对象作为参数
   constructor(options) {
-    //Vue内置属性不用加$符号
-    debugger; // 调试断点，可用于观察实例化过程
-
-    // 将用户传入的所有配置选项保存到实例的$options属性中
     this.$options = options;
-
-    // 根据配置中的el选择器，获取页面上要挂载的目标DOM元素
     this.$el = document.querySelector(options.el);
-
-    // 将用户传入的data数据对象保存到实例的$data属性中
     this.$data = options.data;
-
-    //接收用户定义的方法
-    this.$methods = options.methods;
-
-
-    // 调用初始化方法，启动Vue实例的初始化流程
+    this.$methods = options.methods || {};
     this.init();
   }
 
-  // Vue实例的初始化方法，负责启动各项初始化工作
   init() {
-    debugger;
-    // 1. 执行数据代理，让vm实例可以直接访问$data中的属性（如vm.msg）
-    //vm.msg = options.data.msg
     this.proxyData();
-
-    //2.方法代理    vm.say = options.methods.say
     this.proxyMethods();
-
-    // 3. 执行模板编译，将模板渲染到页面
     this.compile(this.$el);
   }
 
-  // 数据代理方法：实现vm.xxx -> vm.$data.xxx的映射
   proxyData() {
-    // 遍历$data对象上的所有属性名（如msg）
     for (let key in this.$data) {
-      // 使用Object.defineProperty为Vue实例添加同名属性
       Object.defineProperty(this, key, {
-        // 当访问vm.key时触发的getter方法
         get() {
-          // 返回$data中对应属性的值，实现读取代理
           return this.$data[key];
         },
-        // 当给vm.key赋值时触发的setter方法
         set(newVal) {
-          // 将新值赋给$data中对应的属性，实现写入代理
           this.$data[key] = newVal;
-          // 数据更新时重新编译模板
-          this.compile();
+          this.compile(this.$el);
         },
       });
     }
   }
 
   proxyMethods() {
-    //遍历methods对象上的所有方法属性
+    if (typeof this.$methods !== "object") return;
     for (let key in this.$methods) {
-      //把每个方法先绑定死this 然后赋值给this[key]
       this[key] = this.$methods[key].bind(this);
-      
     }
   }
 
-
-  //编译模板 不但能编译自己 还能编译子节点或者说是下级节点
   compile(el) {
+    //声明式渲染  vm = this
+    const vm = this;
     const ELEMENT_NODE = 1;
     const TEXT_NODE = 3;
-    //获取当前元素的子节点
     const { childNodes } = el;
-    //遍历每一个子节点
-    //v-pre 指令表示此节点及其子节点不需要编译
+
     [...childNodes].forEach((node) => {
-      if (node.nodeType === ELEMENT_NODE && node.hasAttribute('v-pre')) {
+      if (node.nodeType === ELEMENT_NODE && node.hasAttribute("v-pre")) {
+        //跳过自己和子节点的编译过程
         return;
       }
-      //如果此子节点的类型是元素节点的话
       if (node.nodeType === ELEMENT_NODE) {
+        //编译此元素节点的属性
+        compileAttributes([...node.attributes],node,vm);
         //编译此子元素节点
-        //获取此元素节点的属性
-        const { attributes } = node;
-        //遍历所有的属性
-        [...attributes].forEach((attr) => {
-          //如果属性名是以v-bind:开头的话
-          if (attr.name.startsWith("v-bind:")) {
-            //v-bind:title
-            //v-bind绑的是属性
-            //获取绑定的属性名
-            const attrName = attr.name.slice(7); //从第7个字符开始截
-            //获取属性的值 title
-            const key = attr.value;
-            //给DOM节点添加属性 setAttribute
-            //node.title = '我是标题属性的值'
-            node.setAttribute(attrName, this[key]);
-          } else if (attr.name.startsWith("v-on:")) {
-            //v-on绑的是事件
-            //v-on:click
-            //获取事件名称
-            const eventName = attr.name.slice(5);
-            //获取事件处理函数的方法名
-            const methodName = attr.value;
-            //给当前节点绑定事件处理函数addEventListener
-            node.addEventListener(eventName, this[methodName]);
+        vm.compile(node);
+        //如果节点是第一个文本子节点的话
+      } else if (node.nodeType === TEXT_NODE) {
+        //先尝试去获取此文本节点上的originalTextContent原始的文本内容
+        let { originalTextContent } = node;
+        //如果没有，则说明是第一次编译此文本节点
+        if (!originalTextContent) {
+          //去除此文本节点的内容
+          originalTextContent = node.textContent;
+          //然后赋给node.originalTextContent属性
+          node.originalTextContent = originalTextContent;
+        }
+
+        //把文本节点的内容中的变量替换为实际数据对象的属性
+        //{{msg}} => hello
+        node.textContent = originalTextContent.replace(
+          /\{\{(.*?)\}\}/g,
+          (_, key) => {
+            return vm[key.trim()];
           }
-        });
-        //递归编译子节点
-        this.compile(node);
-      } //如果此子节点是一个文本子节点的话
-      else if (node.nodeType === TEXT_NODE) {
-        //获取文本节点的内容
-        const { textContent } = node;
-        //使用正则表达式匹配{{}}中的内容 并替换为对应的数据
-        node.textContent = textContent.replace(/\{\{(.*?)\}\}/g, (_, key) => {
-          return this[key.trim()];
-        });
+        );
       }
     });
-    //编译完成后 移除v-cloak属性
-    el.removeAttribute('v-cloak');
+
+     el.removeAttribute("v-cloak");
   }
+}
+
+function compileAttributes(attributes,node,vm) {
+    attributes.forEach((attr) => {
+      if (attr.name === "v-text") {
+        handleVText(node, attr, vm); //node节点 attr属性 vm实例
+      } else if (attr.name === "v-html") {
+        handleVHtml(node, attr, vm); //node节点 attr属性 vm实例
+      }
+      //属性名以v-bind:开头的话
+      if (attr.name.startsWith("v-bind:") || attr.name.startsWith(":")) {
+        handleVBind(node, attr, vm);
+      } else if (attr.name.startsWith("v-on:") || attr.name.startsWith("@")) {
+        handleEvent(node, attr, vm);
+      }
+    });
+}
+
+function handleVText(node,attr,vm){
+    //获取属性的值
+    const expr = attr.value;
+    //从当前的Vue实例属性上获取msg属性的值作为当前元素的文本内容
+    node.textContent = vm[expr];
+}
+
+function handleVHtml(node,attr,vm){
+    //获取属性的值
+    const expr = attr.value;
+    //从当前的Vue实例属性上获取msg属性的值作为当前元素的HTML内容
+    node.innerHTML = vm[expr];
+}
+
+function handleVBind(node, attr, vm) {
+  //获取属性名 :title v-bind:title  
+  let attrName = attr.name;
+  if (attrName.startsWith(':')) {
+    attrName = `v-bind${attrName}`;//把 :title 转成 v-bind:title
+  }
+  //获取绑定的属性名和值
+     attrName = attr.name.slice(7);
+     const key = attr.value;
+     node.setAttribute(attrName, vm[key]);
+}
+
+function handleEvent(node, attr, vm) {
+  //获取属性名 @click v-on:click
+  let attrName = attr.name;
+  if (attrName.startsWith('@')) {
+    attrName = `v-on:${attrName.slice(1)}`; // v-on:click
+  }
+  const eventName = attrName.slice(5);//click
+  const methodName = attr.value;
+  node.addEventListener(eventName, vm[methodName]);
 }
